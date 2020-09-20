b3f6f72b0845424a9f72344e6bd1f0e164eb53c6
dr-spaceman <mat.berti@gmail.com>
2020-09-20 23:00:59 +0800

    React props.children pass props up to parent

--- React.js
+++ React.js
@@ -32,6 +32,37 @@ function Foo(props) {
 }
 const element = <Foo bar='bar' />
 
+// Using props.children to pass props up to parent
+function FancyBorder(props) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + props.color}>
+            {props.children}
+        </div>
+    );
+}
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">Welcome</h1>
+            <p className="Dialog-message">Thank you for visiting our spacecraft!</p>
+        </FancyBorder>
+    );
+}
+// Another example with multiple props
+function SplitPane(props) {
+    return (
+        <div className="SplitPane">
+            <div className="SplitPane-left">{props.left}</div>
+            <div className="SplitPane-right">{props.right}</div>
+        </div>
+    );
+}
+function App() {
+    return (
+        <SplitPane left={<Contacts />} right={<Chat />} />
+    );
+}
+
 /** @STATE **/
 
 // Pure function components are fine!
