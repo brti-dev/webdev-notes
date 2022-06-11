f92d471c9dc8abb296cf37524ac5d4dfb85256ac
Matt Berti <mat.berti@gmail.com>
2022-06-11 21:01:50 +0800

    js random function

--- javascript.js
+++ javascript.js
@@ -243,6 +243,19 @@ Math.round(x) // Returns the value of the number x rounded to the nearest intege
 Math.trunc(x) // Cut off the dot and digits to the right of the integer
 
 /**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
  * @ARRAY
  */
 
