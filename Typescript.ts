c9d97ecbfca0a12f15ead9affe8e308c5f737352
Matt Berti <mat.berti@gmail.com>
2022-06-30 18:09:40 +0800

    Remove redundant example

--- Typescript.ts
+++ Typescript.ts
@@ -327,21 +327,6 @@ const longerString = longest('alice', 'bob')
 const notOK = longest(10, 100) //Argument of type 'number' is not assignable to parameter of type '{ length: number; }'.
 const ok = longest({ value: 10, length: 10 }, { value: 100, length: 100 })
 
-// Function overloads
-// Allow variety of inputs and types in a function
-function makeDate(timestamp: number): Date
-function makeDate(m: number, d: number, y: number): Date
-function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
-  if (d !== undefined && y !== undefined) {
-    return new Date(y, mOrTimestamp, d)
-  } else {
-    return new Date(mOrTimestamp)
-  }
-}
-const d1 = makeDate(12345678)
-const d2 = makeDate(5, 5, 5)
-const d3 = makeDate(1, 3)
-
 /** @UTILITY */
 
 /**
